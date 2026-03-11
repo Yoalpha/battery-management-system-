eaf37d0b1ed92efc9044c30d77046f34f236e973
yogesh naresh <yoyogesh27@gmail.com>
2026-03-11 16:15:14 -0400

    temperature sensor for multiple devices

--- test/temperature_sensor/temperature_sensor.ino
+++ test/temperature_sensor/temperature_sensor.ino
@@ -6,13 +6,48 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+int numberOfDevices;
+
 void setup() {
   Serial.begin(115200);
   sensors.begin();
+
+  numberOfDevices = sensors.getDeviceCount();
+
+  for(int i=0;i<numberOfDevices; i++) {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)) {
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+		} else {
+		  Serial.print("Found ghost device at ");
+		  Serial.print(i, DEC);
+		  Serial.print(" but could not detect address. Check power and cabling");
+		}
+  }
+
+
 }
 
 void loop() {
-  sensors.requestTemperatures();
-  Serial.print("temp:");
-  Serial.println(sensors.getTempCByIndex(0));
+  for(int i =0; i< numberOfDevices; i++){
+    Serial.print("temp for device");
+    Serial.println(i, DEC);
+
+    float temp = sensors.getTempC(tempDeviceAddress);
+    Serial.print(temp);
+  }
+
+  delay(5000);
+
+}
+
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
 }
